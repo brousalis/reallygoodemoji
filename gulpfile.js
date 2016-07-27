46e8448176157436d91a6ce8cfe20983d14bc88c
Pete Brousalis <brousapg@gmail.com>
2016-07-27 16:33:05 -0500

    ghpages deploy

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,11 @@ require('bendystraw')({
   angular: { enabled: true },
   scripts: { babel: true }
 })
+
+var gulp = require('gulp');
+var ghPages = require('gulp-gh-pages');
+
+gulp.task('deploy', function() {
+  return gulp.src('./build/**/*')
+    .pipe(ghPages());
+});
