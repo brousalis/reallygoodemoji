af0ad300bd04b00aab1c9edf4c6a58e600ee67d4
Pete Brousalis <brousapg@gmail.com>
2016-07-27 16:43:55 -0500

    remove hash stuff

--- source/app/app.config.js
+++ source/app/app.config.js
@@ -3,7 +3,6 @@
     .module('app')
     .config(routeConfig)
     .config(imageUrlConfig)
-    .config(urlHashConfig)
     .config(scrollConfig)
 
   function routeConfig($urlRouterProvider, $stateProvider) {
@@ -22,9 +21,5 @@
   function imageUrlConfig($compileProvider) {
     $compileProvider.debugInfoEnabled(true)
     $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|file|blob|emoji):|data:image\//);
-  }
-
-  function urlHashConfig($locationProvider) {
-    $locationProvider.html5Mode({ enabled: true, rewriteLinks: false }).hashPrefix('!');
   }
 }());
